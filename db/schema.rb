a38f0c222e50042069ae2c2b57f0be76a399ad26
jeremiahlukus <jeremiahlukus1@gmail.com>
2017-08-26 14:51:12 -0400

    Added atrubites to devise podcast

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824185144) do
+ActiveRecord::Schema.define(version: 20170826182709) do
 
   create_table "podcasts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20170824185144) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
+    t.string "itunes"
+    t.string "stitcher"
+    t.string "podbay"
     t.index ["email"], name: "index_podcasts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_podcasts_on_reset_password_token", unique: true
   end
