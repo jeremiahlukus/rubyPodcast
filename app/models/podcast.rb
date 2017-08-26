a72bba2c51f70219f912005c77421c831ca96841
jeremiahlukus <jeremiahlukus1@gmail.com>
2017-08-26 16:06:45 -0400

    Added episode models and associations

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -3,4 +3,7 @@ class Podcast < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :eposodes
+
 end
