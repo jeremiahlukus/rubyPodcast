90ae3d90f18050e5776dc48c121d06d4a8def704
jeremiahlukus <jeremiahlukus1@gmail.com>
2017-08-28 00:08:22 -0400

    Changed permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay, :thumbnail) }
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :thumbnail) }
-  end
+  protected
 
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :title, :thumbnail)}
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :thumbnail, :description, :itunes, :stitcher, :podbay)}
+  end
 end
