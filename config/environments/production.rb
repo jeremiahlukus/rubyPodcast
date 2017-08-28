38a838e94bc64191756ee39e31653686e7d98221
jeremiahlukus <jeremiahlukus1@gmail.com>
2017-08-27 21:54:39 -0400

    Added region for aws to fix heroku bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,10 +92,12 @@ Rails.application.configure do
   # sets paperclip to upload to S3 by default
   config.paperclip_defaults = {
     :storage => :s3, 
+    :s3_region => ENV['AWS_REGION'],
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+
     }
   }
 
